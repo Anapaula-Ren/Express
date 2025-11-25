45d77ffa770ac6285c7637e3d68387b7c48ee9ec
Anapaula <L23100201@nlaredo.tecnm.mx>
2025-11-25 07:51:32 -0600

    2ndCommit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/*const express = require('express')
 const app = express()
 const port = 3000
 
@@ -12,4 +12,45 @@ app.post('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})*/
+
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3000
+ 
+app.use(express.json()); //Esta funcion parsea el body de la peticcion y si trae JSON le agrega a php body
+app.use(cors());
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World! Prueba con GET')
 })
+ 
+app.post('/', (req, res) => {
+  res.send('Hello World! Prueba con POST')
+})
+ 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+ 
+app.get('/clientes', cors(), (req, res) => { //cors habilitado solo para esta ruta
+  console.log(req.query);
+  res.send('Hello World! Clientes');
+})
+ 
+app.get('/alumnos', (req, res) => {
+  console.log(req.query);
+  res.send('Hello World! Alumnos');
+})
+ 
+app.get('/docentes/:control', (req, res) => {
+  console.log(req.params);
+  res.send('Hello World! Con num control');
+})
+ 
+app.post('/directivos', (req, res) => {
+  console.log(req.body);
+  res.send('Hello World! Directivos');
+})
+ 
